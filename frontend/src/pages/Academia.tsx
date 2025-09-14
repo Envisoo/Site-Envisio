3e23fd708dc43e2bb515b3483d84dc66f2857242
Envisoo <rodoteopedro@gmail.com>
2025-09-14 01:44:04 +0100

    Descrição das alterações

--- frontend/src/pages/Academia.tsx
+++ frontend/src/pages/Academia.tsx
@@ -34,18 +34,13 @@ const Academia = () => {
     <section className="bg-gray-50">
       <div className="relative">
         {/* Full-screen banner */}
-        <div className="w-full h-[600px] relative">
+        <div>
           <picture>
-            <source
-              media="(max-width: 767px)"
-              srcSet="/academia/banner-mobile.webp"
-            />
-
             <source media="(min-width: 768px)" srcSet="/academia/banner.webp" />
             <img
               src="/academia/banner.webp"
               alt="Banner Academia Envisio"
-              className="w-full h-full object-cover"
+              className="w-full h-[415px] object-cover"
             />
           </picture>
 
