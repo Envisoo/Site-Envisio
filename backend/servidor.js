8e139560f3ee25378b112e8d89fb2672e2bae3ea
Envisoo <rodoteopedro@gmail.com>
2025-10-09 19:53:12 +0100

    Descrição das alterações

--- backend/servidor.js
+++ backend/servidor.js
@@ -3,6 +3,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 
