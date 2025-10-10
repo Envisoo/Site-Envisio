6aa2f5ae020dbd365657a61dc2fe952e37990c4b
Envisoo <rodoteopedro@gmail.com>
2025-10-10 18:30:23 +0100

    Descrição das alterações

--- frontend/src/pages/Academia/src/utils/api.ts
+++ frontend/src/pages/Academia/src/utils/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Cria a instância do Axios com base na URL do .env
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8080", // Alterado para 3001
+  baseURL: process.env.REACT_APP_API_URL || "https://site-envisio-production-9820.up.railway.app", // Alterado para 3001
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
